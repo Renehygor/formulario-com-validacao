17fd730dc0ffe6fd347ccf6e3e157c745c74cbfe
Renehygor <hygorrenebarbosa@gmail.com>
2024-09-19 22:33:22 -0300

    criei um repositorio

--- src/js/index.js
+++ src/js/index.js
@@ -4,24 +4,27 @@ const botao = document.querySelector('.btn-enviar');
 const textos = document.querySelectorAll(".texto")
 
 
-itens.forEach(input => {
 
-    botao.addEventListener('click', () => {
 
-        if (input.value === "") {
-
-            input.classList.add('campo-obrigatorio-nao-preenchido');
-
-            textos.forEach(textos => {
-                textos.classList.add('texto-obrigatorio');
-            });
-
-        } else if (input.value !== '') {
+botao.addEventListener('click', (e) => {
+    e.preventDefault()
+    itens.forEach(input => {
+        if (input.value) {
 
             input.classList.add('campo-obrigatorio-preenchido');
-            textos.forEach(textos => {
-                textos.classList.remove('texto-obrigatorio');
-            });
+            input.nextElementSibling.classList.remove('campo-obrigatorio-nao-preenchido')
+            // textos.forEach(textos => {
+            //     textos.classList.add('texto-obrigatorio');
+            // });
+
+        } else {
+
+            input.classList.remove('campo-obrigatorio-preenchido')
+            input.classList.add('campo-obrigatorio-nao-preenchido')
+            input.classList.add('texto-obrigatorio ')
+            // textos.forEach(textos => {
+            //     textos.classList.remove('texto-obrigatorio');
+            // });
         }
 
     });
